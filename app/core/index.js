f4aabb8738415810dd86d98dba2a1ddb0a6414bc
Pradhaban <pradhabstrikes@gmail.com>
2017-12-02 10:10:16 +0530

    Add Fonts and Design Chnages
    Lib Files

--- app/core/index.js
+++ app/core/index.js
@@ -6,6 +6,7 @@ const {app,BrowserWindow} = electron;
 
 app.on('ready', () => {
     let win = new BrowserWindow({width: 800, height: 600});
+    win.setMenu(null);
     win.loadURL(url.format({
         pathname:path.join(__dirname,'/../../index.html'),
         protocol:'file:',
